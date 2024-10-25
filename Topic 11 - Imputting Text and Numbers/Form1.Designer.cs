bf72ecd3d1e4bda2bb91efff7e00f5724de75eac
Sam Ahrens <339729139@GoTVDSB.ca>
2024-10-25 09:35:50 -0400

    Add project files.

---
+++ Topic 11 - Imputting Text and Numbers/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Topic_11___Imputting_Text_and_Numbers
+{
+    partial class FormInput
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtItem = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.btnCaclulatePrice = new System.Windows.Forms.Button();
+            this.lblItem = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.lblTotalPrice = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtItem
+            // 
+            this.txtItem.Location = new System.Drawing.Point(383, 132);
+            this.txtItem.Name = "txtItem";
+            this.txtItem.Size = new System.Drawing.Size(100, 20);
+            this.txtItem.TabIndex = 0;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(383, 173);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPrice.TabIndex = 1;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(383, 215);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(100, 20);
+            this.txtQuantity.TabIndex = 2;
+            // 
+            // btnCaclulatePrice
+            // 
+            this.btnCaclulatePrice.Font = new System.Drawing.Font("Stencil", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCaclulatePrice.Location = new System.Drawing.Point(289, 241);
+            this.btnCaclulatePrice.Name = "btnCaclulatePrice";
+            this.btnCaclulatePrice.Size = new System.Drawing.Size(194, 81);
+            this.btnCaclulatePrice.TabIndex = 3;
+            this.btnCaclulatePrice.Text = "Calculate Final Price";
+            this.btnCaclulatePrice.UseVisualStyleBackColor = true;
+            this.btnCaclulatePrice.Click += new System.EventHandler(this.btnCaclulatePrice_Click);
+            // 
+            // lblItem
+            // 
+            this.lblItem.AutoSize = true;
+            this.lblItem.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblItem.Location = new System.Drawing.Point(286, 131);
+            this.lblItem.Name = "lblItem";
+            this.lblItem.Size = new System.Drawing.Size(52, 18);
+            this.lblItem.TabIndex = 4;
+            this.lblItem.Text = "Item:";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrice.Location = new System.Drawing.Point(286, 175);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(56, 18);
+            this.lblPrice.TabIndex = 5;
+            this.lblPrice.Text = "Price:";
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuantity.Location = new System.Drawing.Point(286, 217);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(86, 18);
+            this.lblQuantity.TabIndex = 6;
+            this.lblQuantity.Text = "Quantity:";
+            // 
+            // lblTotalPrice
+            // 
+            this.lblTotalPrice.AutoSize = true;
+            this.lblTotalPrice.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalPrice.Location = new System.Drawing.Point(286, 325);
+            this.lblTotalPrice.Name = "lblTotalPrice";
+            this.lblTotalPrice.Size = new System.Drawing.Size(103, 18);
+            this.lblTotalPrice.TabIndex = 7;
+            this.lblTotalPrice.Text = "Final Price:";
+            // 
+            // FormInput
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTotalPrice);
+            this.Controls.Add(this.lblQuantity);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblItem);
+            this.Controls.Add(this.btnCaclulatePrice);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtItem);
+            this.Name = "FormInput";
+            this.Text = "User Input";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtItem;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Button btnCaclulatePrice;
+        private System.Windows.Forms.Label lblItem;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.Label lblTotalPrice;
+    }
+}
+
---
+++ Topic 11 - Imputting Text and Numbers/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Topic_11___Imputting_Text_and_Numbers
+{
+    public partial class FormInput : Form
+    {
+        public FormInput()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCaclulatePrice_Click(object sender, EventArgs e)
+        {
+            string item;
+            int quantity;
+            double price, totalPrice;
+            item = txtItem.Text;
+            quantity = Convert.ToInt32(txtQuantity.Text);
+            price = Convert.ToDouble(txtPrice.Text);
+            totalPrice = (quantity * price * 1.13);
+            lblTotalPrice.Text = "Final Price: " + totalPrice.ToString("C");
+        }
+    }
+}
